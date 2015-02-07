8317108a7a24eb57b48c89db5ecf5847cba6ad58
Charlie Harrison <charlieisharrison@gmail.com>
2015-02-07 16:11:43 -0600

    parse-json

---
+++ parse-json.py
@@ -0,0 +1,17 @@
+import json
+
+if __name__ == "__main__":
+	parsed = []
+	with open('old-banner.json', 'r') as f:
+		s = json.load(f)
+		for item in s:
+			d = item['description']
+
+			n = {
+				'title': item['title'],
+				'code': item['name'],
+				'prereqs': []
+			}
+			parsed.append(n)
+	with open('parsed-json.json', 'w') as f:
+		json.dump(parsed, f)
