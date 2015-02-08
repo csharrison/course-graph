c1ba9402747ac39a4da997c1d9a059ccce8a0bdc
Charlie Harrison <charlieisharrison@gmail.com>
2015-02-07 19:54:21 -0600

    springy layout

--- static/js/drawGraph.js
+++ static/js/drawGraph.js
@@ -31,18 +31,30 @@ $(function() {
         }),
 
     elements: window.elements,
-
-    layout: {
-      name: 'cose',
-      directed: true,
-      roots: '#a',
-      padding: 10
-    }
   });
   var edges = cy.edges()
   var nodes = cy.nodes()
   var connected = edges.connectedNodes();
   cy.remove(nodes.not(connected));
+  cy.layout({
+  name: 'springy',
+
+  animate: true, // whether to show the layout as it's running
+  maxSimulationTime: 10000, // max length in ms to run the layout
+  ungrabifyWhileSimulating: false, // so you can't drag nodes during layout
+  fit: false, // whether to fit the viewport to the graph
+  padding: 30, // padding on fit
+  boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
+  random: false, // whether to use random initial positions
+  infinite: false, // overrides all other options for a forces-all-the-time mode
+  ready: undefined, // callback on layoutready
+  stop: undefined, // callback on layoutstop
+
+  // springy forces
+  stiffness: 400,
+  repulsion: 1000,
+  damping: 0.5
+  })
   console.log('drawing graph');
   var bfs = cy.elements().bfs('#a', function(){}, true);
 });
