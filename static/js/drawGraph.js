4f19931e29c342ea78f674fcbaf8f9eafa3018c9
Alexander Kurihara <alexanderkurihara@alex-kurihara-mba.local>
2015-02-07 19:29:20 -0500

    simple graph drawn

---
+++ static/js/drawGraph.js
@@ -0,0 +1,59 @@
+console.log('hello world!');
+
+$(function() {
+  console.log('on DOM load', document.getElementById('graph')); 
+  var cy = cytoscape({
+    container: document.getElementById('graph'),
+    
+    style: cytoscape.stylesheet()
+      .selector('node')
+        .css({
+          'content': 'data(id)'
+        })
+      .selector('edge')
+        .css({
+          'target-arrow-shape': 'triangle',
+          'width': 4,
+          'line-color': '#ddd',
+          'target-arrow-color': '#ddd'
+        })
+      .selector('.highlighted')
+        .css({
+          'background-color': '#61bffc',
+          'line-color': '#61bffc',
+          'target-arrow-color': '#61bffc',
+          'transition-property': 'background-color, line-color, target-arrow-color',
+          'transition-duration': '0.5s'
+        }),
+    
+    elements: {
+        nodes: [
+          { data: { id: 'a' } },
+          { data: { id: 'b' } },
+          { data: { id: 'c' } },
+          { data: { id: 'd' } },
+          { data: { id: 'e' } }
+        ], 
+        
+        edges: [
+          { data: { id: 'a"e', weight: 1, source: 'a', target: 'e' } },
+          { data: { id: 'ab', weight: 3, source: 'a', target: 'b' } },
+          { data: { id: 'be', weight: 4, source: 'b', target: 'e' } },
+          { data: { id: 'bc', weight: 5, source: 'b', target: 'c' } },
+          { data: { id: 'ce', weight: 6, source: 'c', target: 'e' } },
+          { data: { id: 'cd', weight: 2, source: 'c', target: 'd' } },
+          { data: { id: 'de', weight: 7, source: 'd', target: 'e' } }
+        ]
+      },
+    
+    layout: {
+      name: 'breadthfirst',
+      directed: true,
+      roots: '#a',
+      padding: 10
+    }
+  });
+
+  console.log('drawing graph');
+  var bfs = cy.elements().bfs('#a', function(){}, true);
+});
