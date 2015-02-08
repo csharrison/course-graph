16c749a8ab1ea161fab8de476bbeece1c1e400ca
Charlie Harrison <charlieisharrison@gmail.com>
2015-02-07 18:26:17 -0600

    parseeee

--- parse-json.py
+++ parse-json.py
@@ -2,60 +2,107 @@ import json
 import pprint
 
 def find_prereqs(description, departments, courses):
-	prereqs = 'Prerequisites: '
-	recommended = 'Recommended: '
-	pidx = description.rfind(prereqs) + len(prereqs)
-	ridx = description.rfind(recommended) + len(recommended)
+	prereqs = 'Prerequisite'
+	recommended = 'Recommended'
+	pidx = description.rfind(prereqs)
+	ridx = description.rfind(recommended)
 
-	pre_maybe = description[pidx: description.rfind('.', pidx)]
-	rec_maybe = description[ridx: description.rfind('.', ridx)]
+
+	pre_maybe = None
+	rec_maybe = None
+	if pidx != -1:
+		pre_maybe = description[pidx: description.rfind('.', pidx)]
+	if ridx != -1:
+		rec_maybe = description[ridx: description.rfind('.', ridx)]
+
 
 	def parse(s):
+		if s == None: return []
+
 		r = []
 		words = s.split(' ')
 		last_dep = None
-		for i in xrange(len(words)):
+		i = 0
+		while i  < len(words):
 			if words[i] in departments:
 				try:
 					maybe = words[i] + ' ' + words[i + 1]
 				except IndexError:
+					i += 1
 					continue
 
 				if maybe in courses:
 					r.append(maybe)
 					last_dep = words[i]
+					i +=1
 			elif last_dep and last_dep + ' ' + words[i] in courses:
 				r.append(last_dep + ' ' + words[i])
+			i += 1
+		return r
 	return parse(pre_maybe), parse(rec_maybe)
 
 
 
 if __name__ == "__main__":
-	parsed = []
+	nodes = []
 	departments = set()
-	courses = set()
-	with open('old-banner.json', 'r') as f:
+	courses = {}
+	parsed = {}
+	with open('banner.json', 'r') as f:
 		s = json.load(f)
+		i = 0
 		for item in s:
+			if item['name'].find('CSCI') == -1:
+				continue
 			d = item['description']
 			name = item['name']
 			dep = name.split(' ')[0]
 			if dep not in departments:
 				departments.add(dep)
-			courses.add(name)
 
-			n = {
+			n = { 'data' : {
 				'title': item['title'],
 				'code': name,
 				'description' : d,
 				'prereqs': [],
-				'recommended' : []
-			}
-			parsed.append(n)
-		for item in parsed:
+				'recommended' : [],
+				'id' : str(i)
+			}}
+			i += 1
+			courses[name] = n
+			nodes.append(n)
+
+		edges = []
+		for item in nodes:
+			item = item['data']
 			(pre, rec) = find_prereqs(item['description'], departments, courses)
 			item['prereqs'] = pre
 			item['recommended'] = rec
+			def get_edges(name_lst, t):
+				es = []
+				for p in name_lst:
+					course = courses[p]['data']
+					e = {
+						'data' : {
+							'source': item['id'],
+							'target' : course['id'],
+							'id' : item['id']+'-'+course['id'],
+							'type' : t
+						}
+					}
+					es.append(e)
+				return es
+			edges.extend(get_edges(pre, 'pre'))
+			edges.extend(get_edges(rec, 'rec'))
+
+		parsed = {
+			'nodes' : nodes,
+			'edges' : edges
+		}
+
+
+
+
 
 	with open('parsed-json.json', 'w') as f:
                 pprint.pprint(parsed, f)
